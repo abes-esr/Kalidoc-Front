a5ac99bd0e4ea7bf66ac4424e9e524f135135b38
villiseck <jvk@abes.fr>
2022-06-15 14:11:44 +0200

    #ODS-75 installation prettier pinia typescript

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { loadFonts } from './plugins/webfontloader'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import {createVuetify} from "vuetify";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+const app = createApp(App)
+
+loadFonts()
+
+app.use(vuetify).use(createPinia)
+app.mount('#app')
+
+
+
+
