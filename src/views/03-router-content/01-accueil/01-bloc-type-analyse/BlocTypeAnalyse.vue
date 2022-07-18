44ba9d684d9d5644fdddf203585ff089a3f31814
SamuelQuetin <samquetinuel@gmail.com>
2022-07-18 16:59:31 +0200

    Ajout Radio bouton

--- src/views/03-router-content/01-accueil/01-bloc-type-analyse/BlocTypeAnalyse.vue
+++ src/views/03-router-content/01-accueil/01-bloc-type-analyse/BlocTypeAnalyse.vue
@@ -1,16 +1,26 @@
 <template>
-  <v-card>
+  <v-container>
     <v-card-title>Sélectionner un type d'analyse</v-card-title>
-    
-  </v-card>
+      <v-radio-group v-model="test">
+        <v-radio :label="'Analyse Rapide'" :value="'P1'"></v-radio>
+        <v-radio :label="'Analyse Experte'" :value="'P2'"></v-radio>
+        <v-radio :label="'Analyse Ciblée'" :value="'P3'"></v-radio>
+      </v-radio-group>
+
+    <p>test = {{test}}</p>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: "blocTypeAnalyse"
+  name: "blocTypeAnalyse",
+  data() {
+    return {
+      test: null
+    }
+  }
 };
 </script>
 
 <style scoped>
-
 </style>
