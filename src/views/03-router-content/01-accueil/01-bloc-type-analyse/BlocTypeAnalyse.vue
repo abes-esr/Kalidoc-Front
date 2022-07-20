989d69ad32c524f4a07d00f72ea21f0308f8f3d0
SamuelQuetin <samquetinuel@gmail.com>
2022-07-20 22:24:31 +0200

    Ajout checkbox bouton

--- src/views/03-router-content/01-accueil/01-bloc-type-analyse/BlocTypeAnalyse.vue
+++ src/views/03-router-content/01-accueil/01-bloc-type-analyse/BlocTypeAnalyse.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container>
     <v-card-title>Sélectionner un type d'analyse</v-card-title>
-      <v-radio-group v-model="test">
-        <v-radio :label="'Analyse Rapide'" :value="'P1'"></v-radio>
-        <v-radio :label="'Analyse Experte'" :value="'P2'"></v-radio>
-        <v-radio :label="'Analyse Ciblée'" :value="'P3'"></v-radio>
-      </v-radio-group>
+    <v-radio-group v-model="analyseSelected">
+      <v-radio :label="'Analyse Rapide'" :value="'P1'"></v-radio>
+      <v-radio :label="'Analyse Experte'" :value="'P2'"></v-radio>
+      <v-radio :label="'Analyse Ciblée'" :value="'P3'"></v-radio>
+    </v-radio-group>
+    <v-container fluid v-if="analyseSelected === 'P3'">
+      <v-checkbox v-model="choice1" label="Choix 1"></v-checkbox>
+      <v-checkbox v-model="choice2" label="Choix 2"></v-checkbox>
+      <v-checkbox v-model="choice3" label="Choix 3"></v-checkbox>
+      <v-checkbox v-model="choice4" label="Choix 4"></v-checkbox>
+    </v-container>
 
-    <p>test = {{test}}</p>
+    <p>test = {{ analyseSelected }}</p>
   </v-container>
 </template>
 
@@ -16,7 +22,11 @@ export default {
   name: "blocTypeAnalyse",
   data() {
     return {
-      test: null
+      analyseSelected: null,
+      choice1: null,
+      choice2: null,
+      choice3: null,
+      choice4: null
     }
   }
 };
