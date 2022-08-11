c8da31118bd0f91acf5869710e96efe0da934a84
villiseck <jvk@abes.fr>
2022-08-11 14:06:02 +0200

    Mise en place du bouton de validation

---
+++ src/components/03-accueil-bloc-bouton-recherche/BlocBoutonLancementAnalyse.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-btn depressed color="primary" v-if="dataAreValid">Lancer l'analyse</v-btn>
+    <v-btn depressed v-if="!dataAreValid" disabled>Lancer l'analyse</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BlocBoutonLancementAnalyse",
+  data() {
+    return {
+      dataAreValid: false
+    }
+  }
+};
+</script>
--- src/views/03-router-content/01-accueil/Accueil.vue
+++ src/views/03-router-content/01-accueil/Accueil.vue
@@ -3,6 +3,7 @@
     <bloc-type-analyse></bloc-type-analyse>
     <bloc-recherche-par-ppn></bloc-recherche-par-ppn>
     <bloc-recherche-par-fichier-ppn></bloc-recherche-par-fichier-ppn>
+    <bloc-bouton-lancement-analyse></bloc-bouton-lancement-analyse>
   </v-container>
 </template>
 
@@ -10,12 +11,14 @@
 import BlocTypeAnalyse from "@/components/01-accueil-bloc-type-analyse/BlocTypeAnalyse";
 import BlocRechercheParPpn from "@/components/02-accueil-bloc-recherche-par-ppn/BlocRechercheParPpn";
 import BlocRechercheParFichierPpn from "@/components/02-accueil-bloc-recherche-par-ppn/BlocRechercheParFichierPpn";
+import BlocBoutonLancementAnalyse from "@/components/03-accueil-bloc-bouton-recherche/BlocBoutonLancementAnalyse";
 export default {
   name: "accueil",
   components: {
     BlocTypeAnalyse,
     BlocRechercheParPpn,
-    BlocRechercheParFichierPpn
+    BlocRechercheParFichierPpn,
+    BlocBoutonLancementAnalyse
   }
 };
 </script>
