dc9b9a78fd26adaf36f9a674412178cf88d1f830
villiseck <jvk@abes.fr>
2022-08-18 15:35:25 +0200

    Pinia - Conversion en composition API du bloc recherche par ppn

--- src/components/02-accueil-bloc-recherche-par-ppn/BlocRechercheParPpn.vue
+++ src/components/02-accueil-bloc-recherche-par-ppn/BlocRechercheParPpn.vue
@@ -16,46 +16,55 @@ import { useAnalyseStore } from "@/stores/analyse";
 import { storeToRefs } from "pinia/dist/pinia";
 
 export default {
-  name: "BlocRechercheParPpn",
-  data() {
-    return {
-        comboboxPpnLabel: 'Entrez des PPN ou collez une liste de PPN puis cliquez à l\'extérieur du cadre avec votre souris', //Message indicatif de la combobox
-        comboboxAlert: [], //Alerte indiquant les ppn érronés, et la syntaxe à respecter
-        lastValuesTypedOrPasted: '', //Dernière Chaîne de caractères saisie dans la combobox, servant à alimenter ensuite ppnListTyped
-        ppnListCombobox: [], //Tableau de ppn alimenté par les chaînes de caractères mises dans la combobox
+  setup() {
+    let comboboxPpnLabel = 'Entrez des PPN ou collez une liste de PPN puis cliquez à l\'extérieur du cadre avec votre souris'; //Message indicatif de la combobox
+    let comboboxAlert = []; //Alerte indiquant les ppn érronés, et la syntaxe à respecter
+    let lastValuesTypedOrPasted = ''; //Dernière Chaîne de caractères saisie dans la combobox, servant à alimenter ensuite ppnListTyped
+    let ppnListCombobox = []; //Tableau de ppn alimenté par les chaînes de caractères mises dans la combobox
 
-    }
-  },
-  methods: {
     /**
      * Suppression d'un élément ppn déclenché au moment du clic sur la croix
      * @param item le ppn en question
      */
-    removeItem(item){
+    function removeItem(item){
       const index = this.ppnListCombobox.indexOf(item);
       this.ppnListCombobox.splice(index, 1);
-    },
+    }
+
     /**
      * Suppression de l'ensemble des éléments de la combobox
      */
-    removeAllItems(){
+    function removeAllItems(){
+      //TODO effectuer un controle sur le null du ppnListCombobox
+      console.log('er');
       this.ppnListCombobox = [];
       this.comboboxAlert = [];
-    },
+    }
+
     /**
      * Contrôle des chaînes de caractères saisies dans la combobox à la sortie de la souris du champ et alimentation de ppnListTyped
      */
-    checkValuesAndFeedPpnListTyped(){
-      if(!!this.lastValuesTypedOrPasted){ //Si la valeur n'est pas nulle, ce qui se produit si l'utilisateur sort du cadre sans rien taper
-        let arrayWithValidsPpn = this.lastValuesTypedOrPasted.split(/[^Xx\d]/).filter(ppn_to_check => ppn_to_check.match(/^(\d{8}(\d|X|x))$/));
-        let arrayWithInvalidsPpn = this.lastValuesTypedOrPasted.split(/[^Xx\d]/).filter(ppn_to_check => !ppn_to_check.match(/^(\d{8}(\d|X|x))$/)).filter(str_to_clean => str_to_clean.trim() !== '');
+    function checkValuesAndFeedPpnListTyped(){
+      if(!!lastValuesTypedOrPasted){ //Si la valeur n'est pas nulle, ce qui se produit si l'utilisateur sort du cadre sans rien taper
+        let arrayWithValidsPpn = lastValuesTypedOrPasted.split(/[^Xx\d]/).filter(ppn_to_check => ppn_to_check.match(/^(\d{8}(\d|X|x))$/));
+        let arrayWithInvalidsPpn = lastValuesTypedOrPasted.split(/[^Xx\d]/).filter(ppn_to_check => !ppn_to_check.match(/^(\d{8}(\d|X|x))$/)).filter(str_to_clean => str_to_clean.trim() !== '');
         let arrayWithValidsPpnWithUniqueValues = arrayWithValidsPpn.filter((v, i, a) => a.indexOf(v) === i); //Fonction anonyme de dédoublonnage
         let arrayWithInvalidsPpnWithUniqueValues = arrayWithInvalidsPpn.filter((v, i, a) => a.indexOf(v) === i); //Fonction anonyme de dédoublonnage
-        this.comboboxAlert = [ "Ppn invalides détectés dans ce que vous avez inséré : " + arrayWithInvalidsPpnWithUniqueValues.toString(), "", "" ]
-        arrayWithValidsPpnWithUniqueValues.forEach(currentValidPpn => this.ppnListCombobox.push(currentValidPpn));
+        comboboxAlert = [ "Ppn invalides détectés dans ce que vous avez inséré : " + arrayWithInvalidsPpnWithUniqueValues.toString(), "", "" ]
+        arrayWithValidsPpnWithUniqueValues.forEach(currentValidPpn => ppnListCombobox.push(currentValidPpn));
       }
-      this.lastValuesTypedOrPasted = ''; //On vide la chaîne puisqu'on à alimenté les valeurs valides dans :value="ppnListCombobox"
-    },
+      lastValuesTypedOrPasted = ''; //On vide la chaîne puisqu'on à alimenté les valeurs valides dans :value="ppnListCombobox"
+    }
+
+    return {
+      comboboxPpnLabel,
+      comboboxAlert,
+      lastValuesTypedOrPasted,
+      ppnListCombobox,
+      removeItem,
+      removeAllItems,
+      checkValuesAndFeedPpnListTyped
+    }
   }
 };
 </script>
