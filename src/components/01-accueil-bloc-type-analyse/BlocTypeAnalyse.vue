e3ae4970be677e85b74a2fd98c493105775822bd
SamuelQuetin <samquetinuel@gmail.com>
2022-08-25 16:34:39 +0200

    Refactor: vfor type de doc + vfor type analyse

--- src/components/01-accueil-bloc-type-analyse/BlocTypeAnalyse.vue
+++ src/components/01-accueil-bloc-type-analyse/BlocTypeAnalyse.vue
@@ -2,33 +2,18 @@
   <v-card class="pa-3">
     <v-card-title>Sélectionner un type d'analyse</v-card-title>
     <v-divider></v-divider>
-    <v-radio-group v-model="analyseSelected" @change="updateAnalyseSelectedInStore">
-      <v-tooltip>
+    <v-radio-group style="width: 120px" v-model="analyseSelected" @change="updateAnalyseSelectedInStore">
+      <v-tooltip right v-for="analyse in analysesList" :key="analyse.value">
         <template v-slot:activator="{ on, attrs }">
-          <v-radio :label="'RAPIDE'" :value="'QUICK'" v-bind="attrs" v-on="on"></v-radio>
+          <v-radio :label="analyse.label" :value="analyse.value" v-bind="attrs" v-on="on"></v-radio>
         </template>
-        <span>Règles essentielles / de base</span>
-      </v-tooltip>
-      <v-tooltip top>
-        <template v-slot:activator="{ on, attrs }">
-          <v-radio :label="'EXPERTE'" :value="'COMPLETE'" v-bind="attrs" v-on="on"></v-radio>
-        </template>
-        <span>Règles essentielles / avancées</span>
-      </v-tooltip>
-      <v-tooltip top>
-        <template v-slot:activator="{ on, attrs }">
-          <v-radio :label="'CIBLÉE'" :value="'FOCUS'" v-bind="attrs" v-on="on"></v-radio>
-        </template>
-        <!--        todo changer le LOREM-->
-        <span>Lorem ipsum lorem ipsum</span>
+        <span>{{analyse.bulle}}</span>
       </v-tooltip>
     </v-radio-group>
     <v-container fluid v-if="analyseSelected === 'FOCUS'" >
       <v-card-subtitle >> Par type(s) de documents</v-card-subtitle>
       <v-container class="d-flex flew-row">
-        <v-checkbox v-model="familleDocumentSet" class="pa-1" @change="updateFamilleDocumentSetInStore" value="Choix 1" label="Choix 1"></v-checkbox>
-        <v-checkbox v-model="familleDocumentSet" class="pa-1" @change="updateFamilleDocumentSetInStore" value="Choix 2" label="Choix 2"></v-checkbox>
-        <v-checkbox v-model="familleDocumentSet" class="pa-1" @change="updateFamilleDocumentSetInStore" value="Choix 3" label="Choix 3"></v-checkbox>
+        <v-checkbox v-for="familleDoc in familleDocumentList" :key="familleDoc.value" v-model="familleDocumentSetSelected" class="pa-1" @change="updateFamilleDocumentSetInStore" :value="familleDoc.value" :label="familleDoc.label"></v-checkbox>
       </v-container>
       <v-card-subtitle >> Par jeu(x) de règles préconçu(s) </v-card-subtitle>
       <v-checkbox v-model="ruleSet" value="Choix 1" @change="updateRuleSetInStore" label="Choix 1"></v-checkbox>
@@ -39,7 +24,7 @@
 <!--    todo: supp test -->
     <p>analyse choisie = {{ analyseSelected }}</p>
     <p>Store analyse choisie = {{ analyseStore.getAnalyseSelected }}</p>
-    <p>familleDocumentSet = {{ familleDocumentSet }}</p>
+    <p>familleDocumentSet = {{ familleDocumentSetSelected }}</p>
     <p>Store familleDocumentSet = {{ analyseStore.getFamilleDocumentSet }}</p>
     <p>ruleSet = {{ ruleSet }}</p>
     <p>Store familleDocumentSet = {{ analyseStore.getRuleSet }}</p>
@@ -53,15 +38,95 @@
   import { ref } from 'vue';
 
   const analyseStore = useAnalyseStore();
+  let analysesList = [
+    {
+      label: 'RAPIDE',
+      value: 'QUICK',
+      bulle: "Règles essentielles / de base"
+    },
+    {
+      label: 'EXPERTE',
+      value: 'COMPLETE',
+      bulle: "Règles essentielles / avancées"
+    },
+    {
+      label: 'CIBLÉE',
+      value: 'FOCUS',
+      bulle: "Lorem ipsum lorem ipsum"
+    },
+  ];
+
+  let familleDocumentList = [
+    //Famille AUDIOVISUEL
+    {
+      label: "Audiovisuel",
+      value: "AUDIOVISUEL"
+    },
+    //FAMILLE CARTE
+    {
+      label: "Carte",
+      value: "CARTE"
+    },
+//FAMILLE DOCUMENT ELECTRONIQUE
+    {
+      label: "Doc. Electronique",
+      value: "DOCUMENT ELECTRONIQUE"
+    },
+//FAMILLE ENREGISTREMENT
+    {
+      label: "Enregistrement",
+      value: "ENREGISTREMENT"
+    },
+//FAMILLE IMAGE
+    {
+      label: "Image",
+      value: "IMAGE"
+    },
+//FAMILLE MANUSCRIT
+    {
+      label: "Manuscrit",
+      value: "MANUSCRIT"
+    },
+//FAMILLE MULTIMEDIA
+    {
+      label: "Multimédia",
+      value: "MULTIMEDIA"
+    },
+//FAMILLE MUSIQUE
+    {
+      label: "Musique",
+      value: "MUSIQUE"
+    },
+//FAMILLE OBJET
+    {
+      label: "Objet",
+      value: "OBJET"
+    },
+//FAMILLE PARTITION
+    {
+      label: "Partition",
+      value: "PARTITION"
+    },
+//FAMILLE RESSOURCE CONTINUE
+    {
+      label: "Recource continue",
+      value: "RESSOURCE CONTINUE"
+    },
+//FAMILLE MONOGRAPHIE
+    {
+      label: "Monographie",
+      value: "MONOGRAPHIE"
+    },
+  ];
   let analyseSelected = ref();
-  let familleDocumentSet = ref([]);
+  let familleDocumentSetSelected = ref([]);
   let ruleSet = ref([]);
 
   function updateAnalyseSelectedInStore() {
     analyseStore.setAnalyseSelected(analyseSelected.value);
   }
   function updateFamilleDocumentSetInStore() {
-    analyseStore.setFamilleDocumentSet(familleDocumentSet.value);
+    analyseStore.setFamilleDocumentSet(familleDocumentSetSelected.value);
   }
   function updateRuleSetInStore() {
     analyseStore.setRuleSet(ruleSet.value);
