4fba185a1a3d591c87414ab63645a3d220a94bb9
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-09-02 13:52:01 +0200

    Feat : Ajout du fichier logo200.png Ajout du fichier style.css Modification du fichier Header.vue Modification du fichier index.html Suppression du fichier assistance.png Suppression du fichier documentation.png

--- src/views/01-header/Header.vue
+++ src/views/01-header/Header.vue
@@ -1,19 +1,26 @@
 <template>
-   <v-container class="ma-0 pa-0" style="margin-bottom: 1em" fluid>
-      <v-row style="max-height: 6em; height: 150px" align="center" justify="start" no-gutters>
-         <v-col cols="auto" class="mr-auto" style='padding-left: 2em; padding-right: 2em'>
-            <a><img src="@/assets/logo-periscope.svg" @click="$router.push({path: '/'})" style="max-width: 100%; max-height: 100%" alt="logo periscope" /></a>
+   <v-container class="pa-0 titleBar" fluid>
+      <v-row class="titleBarRow" align="center" justify="start" no-gutters>
+         <v-col cols="auto" class="mr-auto" id="titleBarCols">
+            <a><img src="@/assets/logo200.png" class="titleBarLogo" @click="$router.push({path: '/'})" style="max-width: 100%; max-height: 100%" alt="logo periscope" /></a>
          </v-col>
-         <v-col md="auto">
-            <a href="https://stp.abes.fr/node/3?origine=sudocpro" target="_blank" title="Assistance"><img alt="icone assistance" height="41px" src="@/assets/assistance.png" width="41px" style="margin-left: 1em" /></a>
-         </v-col>
-         <v-col cols="auto" style='padding-left: 2em; padding-right: 2em'>
-            <a href="https://documentation.abes.fr/aideperiscope/" target="_blank" title="Documentation"><img alt="icone documentation" height="41px" src="@/assets/documentation.png" width="41px" style="margin-left: 1em" /></a>
-         </v-col>
+        <v-col cols="auto" class="mr-auto" id="titleBarCols">
+          <v-btn text color="white" value="interfaceVerification" class="titleBarButtonSelected" @click="" v-if="">Interface de vérification</v-btn>
+          <v-btn text color="white" value="interfaceRegles" class="titleBarButtonInactive" @click="" v-if="">Interface des règles</v-btn>
+          <v-btn text color="white" value="historique" class="titleBarButtonInactive" @click="" v-if="">Historique</v-btn>
+        </v-col>
+        <v-col md="auto"><a href="https://stp.abes.fr/node/3?origine=sudocpro" target="_blank" title="Assistance"><v-icon color="white" large>mdi-chat-outline</v-icon></a></v-col>
+        <v-col md="auto" id="titleBarCols"><a href="https://documentation.abes.fr/aideperiscope/" target="_blank" title="Assistance"><v-icon color="white" large>mdi-help</v-icon></a></v-col>
       </v-row>
    </v-container>
 </template>
 
-<script>
-export default {}
+<script setup>
+import { ref } from 'vue';
+
+let isSelected = ref("");
+
+function setSelectedButton(){
+
+}
 </script>
