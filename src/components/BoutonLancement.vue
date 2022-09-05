5f28f0f0cbdbf17029ed0c6b706344e20f627ca9
villiseck <jvk@abes.fr>
2022-09-05 11:52:07 +0200

    Ajout axios

--- src/components/BoutonLancement.vue
+++ src/components/BoutonLancement.vue
@@ -8,14 +8,28 @@
 <script setup>
 
 import {useAnalyseStore} from "@/stores/analyse";
+import axios from "axios";
 
 const analyseStore = useAnalyseStore(); //Store
 
 const props = defineProps({isDisabled: Boolean});
 const emit = defineEmits(["onClick"]);
+const apiUrl = 'http://localhost:8082/api/v1/';
+let json = {"ppnList":["143519379"],"typeAnalyse":"QUICK"};
 
 function launchRequest() {
   console.log("appel axios")
+
+  axios({
+    method: 'post',
+    url: apiUrl + 'check',
+    data: json
+  }).then((response) => {
+    console.log(response)
+  }).catch((error) => {
+    console.log(error);
+  });
+
   //todo: Adapter l'appel des setters avec le retour de l'appel axios
   analyseStore.setNbPpnTotal(5);
   analyseStore.setNbPpnTrouves(9);
