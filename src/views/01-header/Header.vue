1bc532f8ed679e8e437c928d8ef2419c418e54da
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-09-07 11:57:16 +0200

    Feat : Suppression du fichier arrow.png Suppression du fichier bouton_sudoc.png Suppression du fichier icone_sudoc.png Suppression du fichier logo.png Suppression du fichier logo-mesri.png Suppression du fichier logo-periscope.svg Suppression du fichier logoABES.jpg Suppression du fichier MESRI_logo.svg Suppression du fichier periscope.png Ajout du fichier abesLogo130.svg Ajout du fichier MESRLogo.svg Ajout du fichier QMIcon.png Ajout du fichier QMlogoTiny.png Modification du fichier Footer.vue Modification du fichier Header.vue Modification du fichier package.json Modification du fichier package-lock.json Modification du fichier style.css Renommage du fichier logo200.png en QMlogo200.png

--- src/views/01-header/Header.vue
+++ src/views/01-header/Header.vue
@@ -1,114 +1,60 @@
 <template>
-   <v-container absolute padless style="min-height:120px" class="pa-0 titleBar" fluid>
+   <v-container absolute padless class="pa-0 titleBar" fluid>
+     <v-row class="pr-4 pb-4 pl-12" justify="center" align="baseline" no-gutters v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">
+       <v-col cols="2">
+         <v-row class="ma-0 pa-0 pl-2" justify="center">
+           <a><img src="@/assets/QMlogo200.png" class="titleBarNormalSizeLogo" @click="$router.push({path: '/'})" alt="logo Qualimarc" style="position: relative; bottom: -14px"/></a>
+         </v-row>
+       </v-col>
+       <v-col cols="8" class="pl-6">
+         <v-row class="ma-0 pa-0 pl-2" justify="center">
+           <v-btn text tile color="white" value="interfaceVerification" class="titleBarButtonSelected" @click="" v-if="">Interface de vérification</v-btn>
+           <v-btn text tile color="white" value="interfaceRegles" class="titleBarButtonInactive" @click="" v-if="">Interface des règles</v-btn>
+           <v-btn text tile color="white" value="historique" class="titleBarButtonInactive" @click="" v-if="">Historique</v-btn>
+         </v-row>
+       </v-col>
+       <v-col cols="2">
+         <v-row justify="center" no-gutters>
+           <v-col class="ml-2 mr-0 pr-0"><a href="https://stp.abes.fr/node/3?origine=sudocpro" target="_blank" title="Assistance">
+             <v-btn fab small color="white">
+               <v-icon color="#252c61">mdi-forum-outline</v-icon>
+             </v-btn>
+           </a></v-col>
+           <v-col class="ma-0 pl-0" id="titleBarCols"><a href="https://documentation.abes.fr/sudoc/" target="_blank" title="Assistance">
+             <v-btn fab small color="white">
+               <v-icon color="#252c61">mdi-help</v-icon>
+             </v-btn>
+           </a></v-col>
+         </v-row>
+       </v-col>
+     </v-row>
 
-<!--     <v-row-->
-<!--         class="mb-6"-->
-<!--         justify="center"-->
-<!--         no-gutters-->
-<!--     >-->
-<!--       <v-col lg="2">-->
-<!--         <v-card-->
-<!--             class="pa-2"-->
-<!--             outlined-->
-<!--             tile-->
-<!--         >-->
-<!--           1 of 3-->
-<!--         </v-card>-->
-<!--       </v-col>-->
-<!--       <v-col md="auto">-->
-<!--         <v-card-->
-<!--             class="pa-2"-->
-<!--             outlined-->
-<!--             tile-->
-<!--         >-->
-<!--           Variable width content-->
-<!--         </v-card>-->
-<!--       </v-col>-->
-<!--       <v-col lg="2">-->
-<!--         <v-card-->
-<!--             class="pa-2"-->
-<!--             outlined-->
-<!--             tile-->
-<!--         >-->
-<!--           3 of 3-->
-<!--         </v-card>-->
-<!--       </v-col>-->
-<!--     </v-row>-->
-<!--     <v-row no-gutters>-->
-<!--       <v-col>-->
-<!--         <v-card-->
-<!--             class="pa-2"-->
-<!--             outlined-->
-<!--             tile-->
-<!--         >-->
-<!--           1 of 3-->
-<!--         </v-card>-->
-<!--       </v-col>-->
-<!--       <v-col md="auto">-->
-<!--         <v-card-->
-<!--             class="pa-2"-->
-<!--             outlined-->
-<!--             tile-->
-<!--         >-->
-<!--           Variable width content-->
-<!--         </v-card>-->
-<!--       </v-col>-->
-<!--       <v-col lg="2">-->
-<!--         <v-card-->
-<!--             class="pa-2"-->
-<!--             outlined-->
-<!--             tile-->
-<!--         >-->
-<!--           3 of 3-->
-<!--         </v-card>-->
-<!--       </v-col>-->
-<!--     </v-row>-->
-
-<!--     <v-row class="titleBarRow" align="center" justify="start" no-gutters>-->
-<!--       <v-col cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">-->
-<!--         <a><img src="@/assets/logo200.png" class="titleBarLogo" @click="$router.push({path: '/'})" style="max-width: 100%; max-height: 100%" alt="logo Qualimarc" /></a>-->
-<!--       </v-col>-->
-<!--       &lt;!&ndash;        <v-col cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.md && !$vuetify.breakpoint.lg && !$vuetify.breakpoint.xl">&ndash;&gt;-->
-<!--       &lt;!&ndash;          <a><img src="@/assets/logo200.png" class="titleBarLogo" @click="$router.push({path: '/'})" style="max-width: 50%; max-height: 50%" alt="logo Qualimarc" /></a>&ndash;&gt;-->
-<!--       &lt;!&ndash;        </v-col>&ndash;&gt;-->
-<!--       <v-col cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">-->
-<!--         <v-btn text tile color="white" value="interfaceVerification" class="titleBarButtonSelected" @click="" v-if="">Interface de vérification</v-btn>-->
-<!--         <v-btn text tile color="white" value="interfaceRegles" class="titleBarButtonInactive" @click="" v-if="">Interface des règles</v-btn>-->
-<!--         <v-btn text tile color="white" value="historique" class="titleBarButtonInactive" @click="" v-if="">Historique</v-btn>-->
-<!--       </v-col>-->
-<!--       <v-row  no-gutters cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.md && !$vuetify.breakpoint.lg && !$vuetify.breakpoint.xl">-->
-<!--         <v-btn text tile color="white" value="interfaceVerification" class="titleBarButtonSelected" @click="" v-if="">Interface de vérification</v-btn>-->
-<!--         <v-btn text tile color="white" value="interfaceRegles" class="titleBarButtonInactive" @click="" v-if="">Interface des Règles</v-btn>-->
-<!--         <v-btn text tile color="white" value="historique" class="titleBarButtonInactive" @click="" v-if="">Historique</v-btn>-->
-<!--       </v-row>-->
-<!--       <v-row  no-gutters cols="auto" class="mr-auto">-->
-<!--         <v-col md="auto"><a href="https://stp.abes.fr/node/3?origine=sudocpro" target="_blank" title="Assistance"><v-icon color="white" large>mdi-chat-outline</v-icon></a></v-col>-->
-<!--         <v-col md="auto" id="titleBarCols"><a href="https://documentation.abes.fr/aideperiscope/" target="_blank" title="Assistance"><v-icon color="white" large>mdi-help</v-icon></a></v-col>-->
-<!--       </v-row>-->
-<!--     </v-row>-->
-
-      <v-row class="titleBarRow" align="center" justify="start" no-gutters>
-         <v-col cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">
-            <a><img src="@/assets/logo200.png" class="titleBarLogo" @click="$router.push({path: '/'})" style="max-width: 100%; max-height: 100%" alt="logo Qualimarc" /></a>
-         </v-col>
-<!--        <v-col cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.md && !$vuetify.breakpoint.lg && !$vuetify.breakpoint.xl">-->
-<!--          <a><img src="@/assets/logo200.png" class="titleBarLogo" @click="$router.push({path: '/'})" style="max-width: 50%; max-height: 50%" alt="logo Qualimarc" /></a>-->
-<!--        </v-col>-->
-        <v-col cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">
-          <v-btn text tile color="white" value="interfaceVerification" class="titleBarButtonSelected" @click="" v-if="">Interface de vérification</v-btn>
-          <v-btn text tile color="white" value="interfaceRegles" class="titleBarButtonInactive" @click="" v-if="">Interface des règles</v-btn>
-          <v-btn text tile color="white" value="historique" class="titleBarButtonInactive" @click="" v-if="">Historique</v-btn>
-        </v-col>
-        <v-row  no-gutters cols="auto" class="mr-auto" id="titleBarCols" v-if="!$vuetify.breakpoint.md && !$vuetify.breakpoint.lg && !$vuetify.breakpoint.xl">
-          <v-btn text tile color="white" value="interfaceVerification" class="titleBarButtonSelected" @click="" v-if="">Interface de vérification</v-btn>
-          <v-btn text tile color="white" value="interfaceRegles" class="titleBarButtonInactive" @click="" v-if="">Interface des Règles</v-btn>
-          <v-btn text tile color="white" value="historique" class="titleBarButtonInactive" @click="" v-if="">Historique</v-btn>
-        </v-row>
-        <v-row  no-gutters cols="auto" class="mr-auto">
-          <v-col md="auto"><a href="https://stp.abes.fr/node/3?origine=sudocpro" target="_blank" title="Assistance"><v-icon color="white" large>mdi-chat-outline</v-icon></a></v-col>
-          <v-col md="auto" id="titleBarCols"><a href="https://documentation.abes.fr/aideperiscope/" target="_blank" title="Assistance"><v-icon color="white" large>mdi-help</v-icon></a></v-col>
-        </v-row>
-      </v-row>
+     <v-row class="pa-2" justify="center" no-gutters v-if="!$vuetify.breakpoint.md && !$vuetify.breakpoint.lg && !$vuetify.breakpoint.xl">
+       <v-col cols="2" class="pl-8">
+          <a><img src="@/assets/QMIcon.png" class="titleBarNormalSizeLogo" @click="$router.push({path: '/'})" style="height: 50px" alt="logo Qualimarc" /></a>
+       </v-col>
+       <v-col cols="7">
+         <v-row class="ma-0 pa-0" justify="center">
+           <v-btn text tile color="white" value="interfaceVerification" class="titleBarButtonSelected" small @click="" v-if="">Interface de vérification</v-btn>
+           <v-btn text tile color="white" value="interfaceRegles" class="titleBarButtonInactive" small @click="" v-if="">Interface des Règles</v-btn>
+           <v-btn text tile color="white" value="historique" class="titleBarButtonInactive" small @click="" v-if="">Historique</v-btn>
+         </v-row>
+       </v-col>
+       <v-col cols="3">
+         <v-row justify="center" no-gutters>
+           <v-col class="ml-2 mr-0 pr-0"><a href="https://stp.abes.fr/node/3?origine=sudocpro" target="_blank" title="Assistance">
+             <v-btn fab x-small color="white">
+               <v-icon color="#252c61">mdi-forum-outline</v-icon>
+             </v-btn>
+           </a></v-col>
+           <v-col class="ma-0 pl-0" id="titleBarCols"><a href="https://documentation.abes.fr/sudoc/" target="_blank" title="Assistance">
+             <v-btn fab x-small color="white">
+               <v-icon color="#252c61">mdi-help</v-icon>
+             </v-btn>
+           </a></v-col>
+         </v-row>
+       </v-col>
+     </v-row>
    </v-container>
 </template>
 
--- src/views/04-footer/Footer.vue
+++ src/views/04-footer/Footer.vue
@@ -3,21 +3,20 @@
       <v-row class="footerTopRow" align="center" justify="start" no-gutters>
         <v-col cols="auto" lg="8" id="footerDescription" v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">
             <span>
-               Description complète de l'utilité de l'application. "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              <b>Qualimarc</b><br> Description complète de l'utilité de l'application. "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
               exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
             </span>
         </v-col>
          <v-col id="footerAbesLinks">
-            <div><a href="https://documentation.abes.fr/sudoc/" id="linkBlack">Sudoc</a></div>
+           <v-icon>mdi-open-in-new</v-icon>
             <div><a href="https://documentation.abes.fr/aideperiscope/" id="linkBlack">Documentation</a></div>
-            <div><a href="https://stp.abes.fr/node/3?origine=sudocpro" id="linkBlack">ABESstp</a></div>
+            <div><a href="https://stp.abes.fr/node/3?origine=sudocpro" id="linkBlack">Assistance</a></div>
          </v-col>
          <v-col id="footerLogo">
-            <span style='padding-right: 1em'><a href="https://abes.fr/"><img src="@/assets/abes.png" alt="logo abes"/></a></span>
-            <span><a href="https://www.enseignementsup-recherche.gouv.fr/fr"><img src="@/assets/MESRI_logo.svg" alt="logo mesri"/></a></span>
+            <span><a href="https://www.enseignementsup-recherche.gouv.fr/fr"><img src="@/assets/MESRLogo.svg" width="128px" alt="logo mesri"/></a></span>
+            <span style='padding-right: 1em'><a href="https://abes.fr/"><img src="@/assets/abesLogo130.svg" width="100px" alt="logo abes"/></a></span>
          </v-col>
       </v-row>
       <v-row class="footerBottomRow">
@@ -29,9 +28,9 @@
                <!--<a href="" style="color: white">Données personnelles</a> |-->
                <a href="https://abes.fr/pages-cgu/conditions-generales-utilisation-sites-abes.html" id="linkWhite">C.G.U.</a>
                |
-               <a href="https://abes.fr/pages-mentions-legales/periscope.sudoc.html" id="linkWhite">Mentions légales</a>
+               <a href="https://abes.fr/mentions-legales/" id="linkWhite">Mentions légales</a>
                |
-               <a href="https://abes.fr/pages-accessibilite/periscope.html" id="linkWhite">Accessibilité</a>
+               <a href="https://abes.fr/accessibilite/" id="linkWhite">Accessibilité</a>
                <!--<a href="https://status-all.abes.fr/" style="color: white">Disponibilité actuelle de l'application</a> |-->
             </span>
          </v-col>
@@ -40,7 +39,6 @@
 </template>
 
 <script>
-
 export default {
 }
 </script>
