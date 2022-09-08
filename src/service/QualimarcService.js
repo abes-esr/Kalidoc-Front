b7b3c98641d9ab110a396c2dfa5a2a8049e8adcf
SamuelQuetin <samquetinuel@gmail.com>
2022-09-08 11:23:22 +0200

    ajout : - gestion erreur api avec les get du composant Analyse - utilisation du service dans le composant choix analyse - renommer launchrequest -> checkPpnWithTypeAnalyse

--- src/service/QualimarcService.js
+++ src/service/QualimarcService.js
@@ -5,11 +5,27 @@ export class QualimarcService {
         baseURL: process.env.VUE_APP_ROOT_API,
     });
 
-    launchRequest(ppnList, typeAnalyse) {
+  /**
+   * fonction permetant de faire passer les regles sur une liste de ppn selon le type d'analyse choisi
+   * @param ppnList la liste de ppn ex: ["123456789","987654321"]
+   * @param typeAnalyse le type d'analyse choisi ex: "QUICK"
+   * @returns {Promise<AxiosResponse<any>>}
+   */
+    checkPpnWithTypeAnalyse(ppnList, typeAnalyse) {
         return this.client.post('check',{
                 ppnList: ppnList,
                 typeAnalyse: typeAnalyse
         })
     }
+    
+    getFamillesDocuments(){
+      return this.client.get("getFamillesDocuments")
+    }
+
+    getTypesAnalyses(){
+      return this.client.get("getTypesAnalyses")
+    }
+
+
 }
 export default new QualimarcService();
