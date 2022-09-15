43bab8fb1ce26cb6d96b86a7937d4e14869c1cfa
SamuelQuetin <samquetinuel@gmail.com>
2022-09-15 14:36:57 +0200

    ajout data table avec header

---
+++ src/components/resultats/BlocAffichageResume.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <v-card-title>Liste des PPN avec erreurs</v-card-title>
+    <v-divider></v-divider>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+    >
+    </v-data-table>
+  </v-card>
+</template>
+
+<script setup>
+import { ref } from "vue"
+let headers = ref([
+  { text: "Afficher/Masque", value: "masquer"},
+  { text: "PPN", value: "ppn"},
+  { text: "Type", value: "type"},
+  { text: "Nd. erreurs", value: "nberreurs"}
+])
+let items = ref([
+  {
+    masquer : true,
+    ppn : "111111111",
+    type: "doc",
+    nberreurs: 1
+  },
+  {
+    masquer : true,
+    ppn : "222222222",
+    type: "doc",
+    nberreurs: 2
+  },
+  {
+    masquer : false,
+    ppn : "333333333",
+    type: "peri",
+    nberreurs: 102
+  },
+  {
+    masquer : false,
+    ppn : "444444444",
+    type: "media",
+    nberreurs: 1
+  }
+])
+</script>
--- src/views/03-router-content/Resultat.vue
+++ src/views/03-router-content/Resultat.vue
@@ -7,19 +7,9 @@
     <v-icon size="small">mdi-chevron-right</v-icon>
     Résultats de l'analyse
     </v-container>
-<!--    VERSION NAVIGATEUR -->
-    <v-row cols="12" v-if="!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm">
-      <v-col cols="6"><v-card class="secondary" style="min-height: 34em"></v-card></v-col>
-      <v-col cols="6">
-        <v-card class="primary" style="min-height: 18.7em" ></v-card>
-        <bloc-recapitulatif class="pl-1 pr-1" style="min-height: 13em" ></bloc-recapitulatif>
-        <bouton-lancement style="min-height: 2em" @onClick="refreshRecap">Relancer l'analyse</bouton-lancement>
-      </v-col>
-    </v-row>
-<!--    VERSION MOBILE && TABLETTE -->
-    <v-row cols="12" v-if="!$vuetify.breakpoint.md && !$vuetify.breakpoint.lg && !$vuetify.breakpoint.xl">
-      <v-col cols="12"><v-card class="secondary" style="min-height: 34em"></v-card></v-col>
-      <v-col cols="12">
+    <v-row cols="12">
+      <v-col xs="12" sm="12" md="6" lg="6" xl="6"><BlocAffichageResume></BlocAffichageResume></v-col>
+      <v-col xs="12" sm="12" md="6" lg="6" xl="6">
         <v-card class="primary" style="min-height: 18.7em" ></v-card>
         <bloc-recapitulatif class="pl-1 pr-1" style="min-height: 13em" ></bloc-recapitulatif>
         <bouton-lancement style="min-height: 2em" @onClick="refreshRecap">Relancer l'analyse</bouton-lancement>
@@ -30,6 +20,7 @@
 <script setup>
 
 import BlocRecapitulatif from "@/components/resultats/BlocRecapitulatif";
+import BlocAffichageResume from "@/components/resultats/BlocAffichageResume";
 import BoutonLancement from "@/components/BoutonLancement";
 
 function refreshRecap() {
