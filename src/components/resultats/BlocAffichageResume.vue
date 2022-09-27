2f37c6afe04de148dd69a7929b4c5e2d29fa238e
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-09-27 15:45:31 +0200

    Feat :   - modification de la checkbox d'affichage/masquage des lignes de la dataTable

--- src/components/resultats/BlocAffichageResume.vue
+++ src/components/resultats/BlocAffichageResume.vue
@@ -28,18 +28,21 @@
               </v-btn>
             </div>
           </v-menu>
+
         <v-menu v-if="header.value === 'affiche'">
           <template v-slot:activator="{ on, attrs }">
-            <v-checkbox color="#CF4A1A" v-bind="attrs" v-on="on"  on-icon="mdi-eye" off-icon="mdi-eye-off-outline" dense @change="toggleMask"/>
+            <v-btn class="mr-2" fab small depressed color="#676C91" width="20px" height="20px">
+              <v-checkbox class="ma-0 pa-0 pl-2 mt-4" color="#CF4A1A" v-on="on" on-icon="mdi-eye" off-icon="mdi-eye-off-outline" dense @change="toggleMask"></v-checkbox>
+            </v-btn>
           </template>
         </v-menu>
+
         <span style='color: white;'>
           {{ header.text }}
           <v-icon color="white" small >mdi-sort</v-icon></span>
       </template>
       <template v-slot:item.affiche="{ item }">
         <v-simple-checkbox
-
             v-model="item.affiche"
             on-icon="mdi-eye"
             off-icon="mdi-eye-off-outline"
