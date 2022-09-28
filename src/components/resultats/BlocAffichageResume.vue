cd2a116f5998d4d9c984ae8796d1071dde3e90b6
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-09-28 14:55:45 +0200

    Feat :   - modification de la dataTable

--- src/components/resultats/BlocAffichageResume.vue
+++ src/components/resultats/BlocAffichageResume.vue
@@ -2,9 +2,9 @@
 
   <v-container class="ma-0 pa-0">
     <v-row class="ma-0 pa-0">
-      <span style="font-size: 1.26em; color : #252C61; font-weight: bold;">Détail des erreurs par PPN</span>
+      <span class="fontPrimaryColor" style="font-size: 1.26em; font-weight: bold;">Détail des erreurs par PPN</span>
     </v-row>
-    <v-container class="pa-2 ma-0 borderErrorDetailPerPpn">
+    <v-container class="pa-0 ma-0 borderErrorDetailPerPpn">
 
     <v-data-table
       :headers="headers"
@@ -208,6 +208,10 @@ function filterPpnByType(){
     return items.value;
 }
 
+/**
+ * Fonction qui permet d'afficher ou de masquer toutes les lignes
+ * @param value
+ */
 function toggleMask(value) {
   items.value.forEach(item => {
     item.affiche = value;
--- src/components/resultats/BlocDetailPpn.vue
+++ src/components/resultats/BlocDetailPpn.vue
@@ -2,12 +2,12 @@
 
   <v-container class="ma-0 pa-0">
     <v-row class="ma-0 pa-0">
-      <span style="font-size: 1.26em; color : #252C61; font-weight: bold;">Détail des erreurs par PPN</span>
+      <span class="fontPrimaryColor" style="font-size: 1.26em; font-weight: bold;">Détail des erreurs par PPN</span>
     </v-row>
     <v-container class="pa-0 ma-0 borderErrorDetailPerPpn">
-      <img src="@/assets/abesLogo130.svg" style="border: solid 1px black; position: absolute; margin-top: 5px; margin-left: 5px; width: 60px; height: 80px; max-width: 60px; max-height: 80px">
-      <div class="mb-5 text-justify" style="font-size: 1.2em; padding-left: 76px; height: 30px; color: white; background-color: #676C91">Titre du livre / Auteur</div>
-      <div class="mb-5 text-justify fontPrimaryColor" style="font-size: small; padding-left: 76px">Détail des erreurs pour [PPN 03]</div>
+      <img src="@/assets/card-off-outline.svg" alt="Première de couverture non trouvée" class="borderPicturePpnErrorDetail">
+      <div class="mb-2 pt-1 text-justify detailErrorPpnSubtitle" style="background-color: #676C91">{TitreDuLivre} / {Auteur}</div>
+      <div class="mb-2 pt-1 text-justify fontPrimaryColor detailErrorPpnSubtitle">Détail des erreurs pour {PPN}</div>
       <div>
         <v-data-table
             :headers="headers"
@@ -42,25 +42,21 @@
   let itemsPerPage = ref(5);
 
   let headers = ref([
-    {text: "", value: "blank", class: "header"},
-    {text: "Zone UNM1", value: "zone1", class: "header"},
-    {text: "Zone UNM2", value: "zone2", class: "header"},
-    {text: "Message d'erreur (Régle essentielle / Règle avancée)", value: "message", class: "header"}
+    {text: "Zone UNM1", value: "zone1", class: "dataTableHeaderDetailErrorPerPpn"},
+    {text: "Zone UNM2", value: "zone2", class: "dataTableHeaderDetailErrorPerPpn"},
+    {text: "Message d'erreur (Régle essentielle / Règle avancée)", value: "message", class: "dataTableHeaderDetailErrorPerPpn"}
   ]);
   let items = ref([
-    {blank: "", zone1: "210", zone2: "", message: "Zone 210 : doit être remplacée par zone 214"},
-    {blank: "", zone1: "606", zone2: "", message: "Zone 606 : absence de liens $3"},
-    {blank: "", zone1: "700$b", zone2: "", message: "Zone 700 : 700$b contient un terme générique à compléter"},
-    {blank: "", zone1: "", zone2: "210", message: "Zone 210 : doit être remplacée par zone 214"},
-    {blank: "", zone1: "", zone2: "606", message: "Zone 606 : absence de liens $3"},
-    {blank: "", zone1: "", zone2: "700$b", message: "Zone 700 : 700$b contient un terme générique à compléter"},
+    {zone1: "210", zone2: "", message: "Zone 210 : doit être remplacée par zone 214"},
+    {zone1: "606", zone2: "", message: "Zone 606 : absence de liens $3"},
+    {zone1: "700$b", zone2: "", message: "Zone 700 : 700$b contient un terme générique à compléter"},
+    {zone1: "", zone2: "210", message: "Zone 210 : doit être remplacée par zone 214"},
+    {zone1: "", zone2: "606", message: "Zone 606 : absence de liens $3"},
+    {zone1: "", zone2: "700$b", message: "Zone 700 : 700$b contient un terme générique à compléter"},
   ])
 
 </script>
 
 <style scoped>
-.header{
-  color: white;
-  background-color: #e6e6e6;
-}
+
 </style>
