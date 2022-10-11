a4ca1e4cb9d58af56915d4ab10c5e27e9f0b6c52
quetin <quetin@abes.fr>
2022-10-11 11:12:19 +0200

    fix: fichier excel mauvais format

--- src/views/03-router-content/Resultat.vue
+++ src/views/03-router-content/Resultat.vue
@@ -17,7 +17,7 @@
           <v-tooltip left>
             <template v-slot:activator="{on}" class="ma-0 pa-0 col-auto">
                 <v-btn :disabled="itemsToExport().length === 0" style="position: absolute; top: 4px; right: -10px; margin-right: 12px;" class="button" v-on="on" color="#0F75BC">
-                  <download-csv :delimiter="';'" :data="itemsToExport()" name="qualimarc-export.csv" separator-excel>
+                  <download-csv :delimiter="';'" :data="itemsToExport()" name="qualimarc-export.csv">
                     TÉLÉCHARGER TOUS<br/>
                     LES RESULTATS
                   </download-csv>
