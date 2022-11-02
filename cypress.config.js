fd675814cbf938be31b6d28504267f190ca99766
villiseck <jvk@abes.fr>
2022-11-02 16:14:50 +0100

    Github integration debut module export

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "i4mqah",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
-
-module.exports = {
-  projectId: "i4mqah"
-  // The rest of the Cypress config options go here...
-}
