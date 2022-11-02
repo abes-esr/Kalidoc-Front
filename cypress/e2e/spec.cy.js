2b5a088b4af14ea652480020ca7b4d793a692bd6
SamuelQuetin <samquetinuel@gmail.com>
2022-11-02 14:01:33 +0100

    ajout intercept

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,13 @@
 describe('First Test', () => {
   it('Nothing to do', () => {
-    cy.visit('http://localhost:8080')
+    //Mock
+    cy.intercept("http://localhost:8082/api/v1/getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
+    cy.intercept("http://localhost:8082/api/v1/getRuleSets",[]);
 
-    cy.contains('type').click()
+    cy.visit('http://localhost:8080');
+
+    cy.contains("CIBL").click();
+    cy.contains("Audiovisuel").click();
+    // cy.contains("LANCER").should('be.disabled');
   })
 })
