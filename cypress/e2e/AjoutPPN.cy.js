a3da442b4102d6e32ea0ea9694bf54107479649e
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-11-03 09:36:01 +0100

    Feat :     - ajout d'un test sur l'ajout de ppn et le vidage de la liste

---
+++ cypress/e2e/AjoutPPN.cy.js
@@ -0,0 +1,64 @@
+describe('Ajout de ppn et vidage de la liste', () => {
+    it('Nothing to do', () => {
+        //Mock
+        cy.intercept("http://localhost:8082/api/v1/getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
+        cy.intercept("http://localhost:8082/api/v1/getRuleSets",[]);
+
+        cy.viewport(1000, 1000);
+
+        cy.visit('http://localhost:8080');
+
+        // -------------------------------------
+        // PREMIER TEST
+        // -------------------------------------
+
+        // Teste l'ajout d'un ppn par ppn
+        cy.get('.v-select__selections').click().type("123456789 ");
+        cy.get('.v-select__selections').click().type("123456790 ");
+        cy.get('.v-select__selections').click().type("45df753");
+        cy.get('.v-select__selections').type('{enter}');
+
+        // Teste des valeurs de chaque chip
+        cy.get(':nth-child(1) > .v-chip__content').contains("123456789");
+        cy.get(':nth-child(2) > .v-chip__content').contains("123456790");
+
+        // Teste de la visibilité du v-alert
+        cy.get('.v-alert').should('not.be.hidden');
+
+        // Ouvre le v-alert
+        cy.get('.v-expansion-panel-header__icon > .v-icon').click();
+
+        // Teste de la valeur du chip dans le v-alert
+        cy.get('.v-expansion-panel-content__wrap > .v-chip > .v-chip__content').contains("45df753");
+
+        // Teste le vidage de la liste
+        cy.get('.pe-1 > .v-btn__content').click();
+        cy.get('.v-select__selections').should('have.value', '');
+
+
+        // -------------------------------------
+        // DEUXIEME TEST
+        // -------------------------------------
+
+        // Teste l'ajout d'une liste de ppn
+        cy.get('.v-select__selections').click().type("123456789,123456790 45df753");
+        cy.get('.v-select__selections').type('{enter}');
+
+        // Teste la valeur de chaque ppn
+        cy.get(':nth-child(1) > .v-chip__content').contains("123456789");
+        cy.get(':nth-child(2) > .v-chip__content').contains("123456790");
+
+        // Teste de la visibilité du v-alert
+        cy.get('.v-alert').should('not.be.hidden');
+
+        // Ouvre le v-alert
+        cy.get('.v-expansion-panel-header__icon > .v-icon').click();
+
+        // Teste de la valeur du chip dans le v-alert
+        cy.get('.v-expansion-panel-content__wrap > .v-chip > .v-chip__content').contains("45df753");
+        // Teste le vidage de la liste
+        cy.get('.pe-1 > .v-btn__content').click();
+        cy.get('.v-select__selections').should('have.value', '');
+
+    })
+})
