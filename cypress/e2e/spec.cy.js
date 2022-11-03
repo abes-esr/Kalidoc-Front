5f36b45f5e6702346265edba5bb0ccfbbcd3b79f
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-11-03 09:11:46 +0100

    Feat :     - suppression d'un id btnClearAll dans le composant BlockRechercheParPpn.vue     - modification du controle sur le vidage de la combobox

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,11 +13,12 @@ describe('First Test', () => {
     cy.get('.button').should('be.disabled');
 
     cy.get('.v-select__selections').click().type("123456789");
-    cy.get('.v-select__selections').contains("123456789");
     cy.contains("RAPIDE").click();
     cy.get('.button').should('be.enabled');
 
-    cy.get('[id^=btnClearAll]').click();
+    cy.get('.v-chip__content > .pr-2').contains("123456789");
+
+    cy.get('.pe-1 > .v-btn__content').click();
     cy.get('.v-select__selections').should('have.value', '');
   })
 })
