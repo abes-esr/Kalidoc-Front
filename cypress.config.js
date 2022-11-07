c4994af4e620cd437d43f82a2eabeef48de1e916
villiseck <jvk@abes.fr>
2022-11-07 15:26:17 +0100

    modif ajout env

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "i4mqah",
+  env: {
+    url: 'http://localhost:8080/',
+    urlApi: 'https://qualimarc-dev.sudoc.fr/api/v1/',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
 describe('First Test', () => {
   it('Nothing to do', () => {
     //Mock
-    cy.intercept("https://qualimarc-dev.sudoc.fr/api/v1/getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
-    cy.intercept("https://qualimarc-dev.sudoc.fr/api/v1/getRuleSets",[]); 
+    cy.intercept(Cypress.env('urlApi') + "getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
+    cy.intercept(Cypress.env('urlApi') + "getRuleSets",[]); 
 
     cy.visit('http://localhost:8080');
 
