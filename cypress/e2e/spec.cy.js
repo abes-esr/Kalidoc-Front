4a358ffbedf7002775ea8164b9ac5900a0acbbbe
villiseck <jvk@abes.fr>
2022-11-07 15:15:18 +0100

    minor changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe('First Test', () => {
   it('Nothing to do', () => {
     //Mock
     cy.intercept("https://qualimarc-dev.sudoc.fr/api/v1/getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
-    cy.intercept("https://qualimarc-dev.sudoc.fr/api/v1/getRuleSets",[]);
+    cy.intercept("https://qualimarc-dev.sudoc.fr/api/v1/getRuleSets",[]); 
 
     cy.visit('http://localhost:8080');
 
