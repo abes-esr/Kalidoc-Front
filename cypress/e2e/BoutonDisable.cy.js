799b2850669881e8495f24304c46127844c9e428
SamuelQuetin <samquetinuel@gmail.com>
2022-11-07 22:08:02 +0100

    part1

--- cypress/e2e/BoutonDisable.cy.js
+++ cypress/e2e/BoutonDisable.cy.js
@@ -1,13 +1,5 @@
 describe('First Test', () => {
   it('Nothing to do', () => {
-    //Mock
-    cy.intercept("http://localhost:8082/api/v1/getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
-    cy.intercept("http://localhost:8082/api/v1/getRuleSets",[]);
-
-    cy.viewport(1000, 1000);
-
-    cy.visit('http://localhost:8080');
-
     cy.contains("CIBL").click();
     cy.contains("Audiovisuel").click();
     cy.get('.button').should('be.disabled');
