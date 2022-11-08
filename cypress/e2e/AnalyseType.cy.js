5ba185998b0b212284b6dddd8585ad7df1da171e
quetin <quetin@abes.fr>
2022-11-08 14:58:32 +0100

    Refactor enlever le before + renommage

--- cypress/e2e/AnalyseType.cy.js
+++ cypress/e2e/AnalyseType.cy.js
@@ -1,20 +1,3 @@
-before(() => {
-    // Mock
-    cy.intercept("http://localhost:8082/api/v1/getFamillesDocuments",[{"id":"B","libelle":"Audiovisuel"}]);
-    cy.intercept("http://localhost:8082/api/v1/getRuleSets",[]);
-
-    // Réglage de la taille de la fenêtre
-    cy.viewport(1400, 1000);
-
-    // Affichage de la home page
-    cy.visit('http://localhost:8080');
-})
-
-beforeEach(() => {
-    // Réglage de la taille de la fenêtre
-    cy.viewport(1400, 1000);
-})
-
 describe('Sélection du type d\'analyse', () => {
     it('Analyse RAPIDE', () => {
         cy.get('.v-input--radio-group__input > :nth-child(1)').click();
