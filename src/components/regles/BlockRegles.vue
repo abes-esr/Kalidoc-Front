9c09b355b87448487f3e7bc1683cf82dad26d77c
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-11-10 11:22:57 +0100

    Feat :     - ajout du filtre sur les types de document

--- src/components/regles/BlockRegles.vue
+++ src/components/regles/BlockRegles.vue
@@ -26,7 +26,7 @@
                             </span>
 
                 <!--                Champ de recherche pour la colonne "Règles de vérification / qualité"-->
-                <v-menu offset-y v-if="header.value === 'recherche'">
+                <v-menu offset-y v-if="header.value === 'message'">
                   <template v-slot:activator="{ on, attrs }">
                     <v-text-field dense label="Rechercher (saisir un mot)" single-line solo v-bind="attrs" v-on="on" style="margin-bottom: -20px">
                       Rechercher (saisir un mot)
@@ -35,7 +35,7 @@
                 </v-menu>
 
                 <!--                Icônes de tri pour les ID, les types de documents et les types de règles-->
-                <v-menu offset-y v-if="header.value === 'id' || header.value === 'typeDocument' || header.value === 'typeRegle'">
+                <v-menu offset-y v-if="header.value === 'id' || header.value === 'typeDoc' || header.value === 'priority'">
                   <template v-slot:activator="{ on, attrs }">
                     <v-btn text class="bouton-simple" x-small v-bind="attrs" v-on="on" style="text-decoration: none;">
                       <v-icon small color="white">
@@ -43,10 +43,15 @@
                       </v-icon>
                     </v-btn>
                   </template>
+                  <div style='background-color:white;color: black;' class="pl-4 pr-8" v-if="header.value === 'typeDoc'">
+                    <v-btn class="d-block" plain v-for="type in selectType" :key="type.id" @click="eventTypeChoice(type)">
+                      {{ type }}
+                    </v-btn>
+                  </div>
                 </v-menu>
 
                 <!--                Suppression de l'icône de tri pour la colonne "Règle de vérification / qualité"-->
-                <v-icon color="white" small v-if="header.value != 'recherche'">mdi-sort</v-icon>
+                <v-icon color="white" small v-if="header.value != 'message'">mdi-sort</v-icon>
               </template>
 
             </v-data-table>
@@ -59,21 +64,29 @@
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {ref, onMounted} from "vue";
+import QualimarcService from "@/service/QualimarcService";
 
+const serviceApi = QualimarcService;
 let headers = ref([
   { text: "ID Règle", value: "id", class: "headerTableClass", width: 20},
-  { text: "Zone UNM 1", value: "unm1", class: "headerTableClass", width: 30},
-  { text: "Zone UNM 2", value: "unm2", class: "headerTableClass", width: 30},
-  { text: "Type de document", value: "typeDocument", class: "headerTableClass", width: 80},
-  { text: "Règle de vérification / qualité", value: "recherche", class: "headerTableClass", width: 200, sortable : false},
-  { text: "Type de règle", value: "typeRegle", class: "headerTableClass", width: 50}
+  { text: "Zone UNM 1", value: "zoneUnm1", class: "headerTableClass", width: 30},
+  { text: "Zone UNM 2", value: "zoneUnm2", class: "headerTableClass", width: 30},
+  { text: "Type de document", value: "typeDoc", class: "headerTableClass", width: 80},
+  { text: "Règle de vérification / qualité", value: "message", class: "headerTableClass", width: 200, sortable : false},
+  { text: "Type de règle", value: "priority", class: "headerTableClass", width: 50}
 ]);
 let items = ref([
-  {id: "01", unm1:"210", unm2: "", typeDocument: "Tous", recherche: "Si présence de zone 210 et absence de 214", typeRegle: "Essentielle"},
-  {id: "02", unm1:"606", unm2: "", typeDocument: "Tous", recherche: "Zone 606 : absence de liens $3", typeRegle: "Essentielle"},
-  {id: "03", unm1:"700$b", unm2: "", typeDocument: "Tous", recherche: "Zone 700 : 700$b contient un terme générique à compléter", typeRegle: "Avancée"},
+  {id: "01", zoneUnm1:"210", zoneUnm2: "", typeDoc: "Tous", message: "Si présence de zone 210 et absence de 214", priority: "Essentielle"},
+  {id: "02", zoneUnm1:"606", zoneUnm2: "", typeDoc: "Tous", message: "Zone 606 : absence de liens $3", priority: "Essentielle"},
+  {id: "03", zoneUnm1:"700$b", zoneUnm2: "", typeDoc: "Tous", message: "Zone 700 : 700$b contient un terme générique à compléter", priority: "Avancée"},
 ]);
+let selectType = ref([]);
+let rulesFiltered = [];
+
+onMounted(() => {
+  feedTypeList();
+})
 
 /**
  * Fonction qui modifie la class de l'item sélectionné en fonction de sa priorité
@@ -82,9 +95,41 @@ let items = ref([
  */
 function classItemPriority(item){
   return {
-    essentielle: item.typeRegle === 'Essentielle',
-    avancee: item.typeRegle === 'Avancée',
+    essentielle: item.priority === 'Essentielle',
+    avancee: item.priority === 'Avancée',
   }
+}
+
+/**
+ * Fonction permettant d'initialiser les listes de types de documents affichés dans le filtre
+ */
+function feedTypeList() {
+  selectType.value.push("Tous");
+  serviceApi.getFamillesDocuments()
+      .then((response) => {
+        response.data.forEach((el) => selectType.value.push(el.libelle));
+      }).catch((error) => {
+  });
+}
+
+/**
+ * Fonction qui permet d'afficher les types de document
+ * @param element
+ * @returns {*[] | []}
+ */
+function eventTypeChoice(element) {
+  type.value = (element === "Tous") ? null : element;
+  return filterPpnByType();
+}
+
+function filterPpnByType(){
+  if (type.value !== null) {
+    rulesFiltered = items.value.filter(item => {
+      return item.typeDocument === type.value;
+    });
+    return rulesFiltered;
+  }
+  return items.value;
 }
 
 </script>
