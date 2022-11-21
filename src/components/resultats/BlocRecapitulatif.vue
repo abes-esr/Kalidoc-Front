b79612f6a254b935c14e48962ca7b23d08ccf94f
SamuelQuetin <samquetinuel@gmail.com>
2022-11-21 15:12:09 +0100

    Mise en place du carousel et gestion du nombre de slide

--- src/components/resultats/BlocRecapitulatif.vue
+++ src/components/resultats/BlocRecapitulatif.vue
@@ -6,87 +6,51 @@
     <v-container class="pa-0 ma-0 borderErrorDetailPerPpn">
       <span style="color: grey; font-weight: 400; font-size: 0.9em;"><bloc-rappel-type-analyse></bloc-rappel-type-analyse></span>
 
-<!--            MENU RECAPITULATIF DES RESULTATS DES RECHERCHES EFFECTUEES -->
-      <v-tabs v-model="tab" background-color="white" color="black" grow show-arrows>
-        <v-tabs-slider color="#252C61"></v-tabs-slider>
-        <v-tab v-for="item in items" :key="item" style="font-size: 0.7em;">
-          {{ item }}
-        </v-tab>
-      </v-tabs>
-<!--            AFFICHAGE DES RESULTATS DES RECHERCHES EFFECTUEES -->
-      <v-tabs-items v-model="tab">
-        <v-tab-item v-for="item in items" :key="item">
-          <v-card flat>
-            <v-card-text>
-<!--                    NOMBRE TOTAL DE PPN ANALYSES -->
-              <v-container class="d-flex justify-space-between pt-0 pb-1" style="width: 24em"><span>Nb. total de PPN analysés</span>
-                <v-chip class="numberValue" small label disabled text-color="white" color="#0F75BC">{{
-                    getNbPpnTotal()
-                  }}
-                </v-chip>
-              </v-container>
-<!--                    NOMBRE DE PPN AVEC ERREURS -->
-              <v-container class="d-flex justify-space-between pt-0 pb-1" style="width: 24em"><span>Nb. de PPN avec erreurs</span>
-                <v-chip class="numberValue" small label disabled text-color="white" color="#0F75BC" >{{
-                    getNbPpnErreurs()
-                  }}
-                </v-chip>
-              </v-container>
-<!--                    NOMBRE DE PPN SANS ERREURS -->
-              <v-container class="d-flex justify-space-between pt-0 pb-1" style="width: 24em"><span>Nb. de PPN sans erreurs</span>
-                <v-chip class="numberValue" small label disabled text-color="white" color="#0F75BC">{{
-                    getNbPpnOk()
-                  }}
-                </v-chip>
-              </v-container>
-<!--                    NOMBRE DE PPN NON TROUVES -->
-              <v-container class="d-flex justify-space-between pt-0 pb-1" style="width: 24em"><span>Nb. de ppn non trouvés</span>
-                <v-chip class="numberValue" small label disabled text-color="white" color="#0F75BC">{{
-                    getNbPpnInconnus()
-                  }}
-                </v-chip>
-              </v-container>
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs-items>
+      <v-carousel
+          hide-delimiters
+          :continuous="false"
+      >
+        <v-carousel-item
+            v-for="(i) in getNumberSlides()"
+            :key="i"
+        >
+          <v-row>
+            <v-col>
+              <CardRecapitulatif :numero-lancement="i"/>
+            </v-col>
+            <v-col v-if="isItemsSizeGreaterThanOneAndLesserThanSize(i)">
+              <CardRecapitulatif :numero-lancement="i+1"/>
+            </v-col>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
     </v-container>
+    <v-btn @click="test()">test</v-btn>
   </v-container>
 </template>
 
 <script setup>
-
 import BlocRappelTypeAnalyse from "@/components/resultats/BlocRappelTypeAnalyse";
-import { useResultatStore } from "@/stores/resultat";
+import CardRecapitulatif from "@/components/resultats/CardRecapitulatif";
 import {ref} from 'vue';
 
-const resultatStore = useResultatStore();
-let tab = ref(null);
-let items = ref(['Analyse 1', 'Analyse 2', 'Analyse 3', 'Analyse 4', 'Analyse 5']);
-let text = ref('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
+let items = ref();
 
-function getNbPpnTotal() {
-  return resultatStore.getNbPpnTotal;
+items.value = ["1"]
+
+
+function test() {
+  items.value.push("2");
 }
 
-function getNbPpnInconnus() {
-  return resultatStore.getNbPpnInconnus;
+function getNumberSlides() {
+  console.log(items.value.length);
+  return items.value.length === 1 ? 1 : items.value.length - 1;
 }
 
-function getNbPpnErreurs() {
-  return resultatStore.getNbPpnErreurs;
-}
-
-function getNbPpnOk() {
-  return resultatStore.getNbPpnOk;
+function isItemsSizeGreaterThanOneAndLesserThanSize(i) {
+  return (items.value.length > 1) && (i < items.value.length);
 }
 
 </script>
 
-<style scoped>
-.numberValue {
-  width: 59px;
-  opacity: unset;
-  justify-content: right;
-}
-</style>
