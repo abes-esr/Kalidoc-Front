bbcc8a97c2bb3ec8cdf3a80287efd99bac1fd05b
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-11-29 15:36:17 +0100

    Refactor :    - changement de la couleur des icone de tri lorsqu'une sélection est effectuée

--- src/components/regles/BlockRegles.vue
+++ src/components/regles/BlockRegles.vue
@@ -178,19 +178,19 @@ function resetSelector() {
 function colorIconFilterTypeDoc() {
   if (selectedCheckbox.value[0] === "Tous" || selectedCheckbox.value === "Tous" || selectedCheckbox.value.length === 0) {
     return 'white';
-  } else return '#e69275';
+  } else return '#FFC1AB';
 }
 
 function colorIconFilterId() {
   if (selectedId.value === "Tous") {
     return 'white';
-  } else return '#e69275';
+  } else return '#FFC1AB';
 }
 
 function colorIconFilterPriority() {
   if (selectedPriority.value === "Toutes") {
     return 'white';
-  } else return '#e69275';
+  } else return '#FFC1AB';
 }
 
 /**
