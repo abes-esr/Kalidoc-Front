aebc61ded05004e1cb9171054baa9df39f5f7e9e
quetin <quetin@abes.fr>
2022-11-29 15:46:14 +0100

    tentative pas ouf

---
+++ src/plugins/theme.js
@@ -0,0 +1,12 @@
+export default{
+    dark: false,
+    colors: {
+        background: '#F9F9FB',
+        primary: '#252c61',
+        secondary: '#00A0DC',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    }
+}
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,19 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 import fr from 'vuetify/lib/locale/fr';
+import themeQualimarc from './theme';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        options: {
+            customProperties: true,
+        },
+        themes: {
+            themeQualimarc
+        }
+    },
     lang: {
       locales: { fr },
       current: 'fr',
