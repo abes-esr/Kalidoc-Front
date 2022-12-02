ef2586c211963bdfa50f48443407f088205d2318
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-12-02 09:24:52 +0100

    Fix :    - correction du scrollbar

--- src/components/historique/BlocHistorique.vue
+++ src/components/historique/BlocHistorique.vue
@@ -10,7 +10,7 @@
         <v-icon color="white" small>mdi-download</v-icon>
       </v-btn>
     </v-row>
-    <v-row class="ma-0 pa-0" justify="center" style="border-top: 4px solid #252c61">
+    <div class="ma-0 pa-0" style="border-top: 4px solid #252c61">
       <v-row class="mt-1" justify="space-around">
         <v-alert class="mt-4" border="left" colored-border dense type="warning" elevation="2" icon="mdi-alert">
           L’historique n’est valable que pour la durée de la session.
@@ -44,35 +44,34 @@
               <!--      EXPANSION PANEL CONTENT      -->
               <v-expansion-panel-content>
                 <v-divider></v-divider>
-                <v-row class="mt-2">
-                  <!--      TRAITEMENT PAR RESULTAT     -->
-                  <v-col v-for="result in analyse.resultats.slice().reverse()">
-                    <v-row class="d-flex align-center">
-                      <!--      AFFICHAGE DU NUMERO DU RESULTAT      -->
-                      <v-col cols="1" style="color: lightgrey; font-weight: 800; font-size: 1.2em">{{ analyse.resultats.indexOf(result) +1 }}</v-col>
-                      <v-col class="d-flex align-start flex-column ma-0 pa-0 pl-2" cols="5" style="border-left: 2px solid lightgrey">
-                          <span>Nb. total de PPN analysés : </span>
-                          <span>Nb. de PPN avec erreurs : </span>
-                          <span>Nb. de PPN sans erreurs : </span>
-                          <span>Nb. de ppn non trouvés : </span>
-                      </v-col>
-                      <v-col class="d-flex align-start flex-column ma-0 pa-0" cols="2">
-                        <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnTotal.length }}</v-btn>
-                        <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnErreurs.length }}</v-btn>
-                        <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnOk.length }}</v-btn>
-                        <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnInconnus.length }}</v-btn>
-                      </v-col>
-                    </v-row>
-                  </v-col>
-                </v-row>
+                  <v-list-item class="mt-2" style="overflow-x: scroll; overflow-y: hidden">
+                    <!--      TRAITEMENT PAR RESULTAT     -->
+                    <v-col v-for="result in analyse.resultats.slice().reverse()">
+                      <v-row class="d-flex align-center" style="min-width: 330px">
+                        <!--      AFFICHAGE DU NUMERO DU RESULTAT      -->
+                        <v-col style="color: lightgrey; font-weight: 400; font-size: 1.2em; width: 40px; max-width: 40px">{{ analyse.resultats.indexOf(result) +1 }}</v-col>
+                        <v-col class="d-flex align-start flex-column ma-0 pa-0 pl-2" style="border-left: 2px solid lightgrey; width: 220px; max-width: 220px">
+                          <span>Nb. total de PPN analysés :</span>
+                          <span>Nb. de PPN avec erreurs :</span>
+                          <span>Nb. de PPN sans erreurs :</span>
+                          <span>Nb. de ppn non trouvés :</span>
+                        </v-col>
+                        <v-col class="d-flex align-start flex-column ma-0 pa-0" style="width: 70px; max-width: 70px">
+                          <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnTotal.length }}</v-btn>
+                          <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnErreurs.length }}</v-btn>
+                          <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnOk.length }}</v-btn>
+                          <v-btn class="mb-1" x-small color="blue">{{ result.nbPpnInconnus.length }}</v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                  </v-list-item>
               </v-expansion-panel-content>
             </v-expansion-panel>
           </v-expansion-panels>
         </v-timeline-item>
-
       </v-timeline>
 
-    </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -81,7 +80,7 @@ import {ref} from "vue";
 
 let analyseList = [
   {
-    date: "2022/12/01 15h42",
+    date: "2022/12/01 13h12",
     analyse: {
       ppnValidsList: [],
       ppnInvalidsList: [],
@@ -101,11 +100,53 @@ let analyseList = [
         nbPpnInconnus: [2,2],
         nbPpnErreurs: [2,2],
         nbPpnOk: [2,2],
+      },
+      {
+        nbPpnTotal: [3,3,3],
+        nbPpnInconnus: [3,3,3],
+        nbPpnErreurs: [3,3,3],
+        nbPpnOk: [3,3,3],
+      },
+      {
+        nbPpnTotal: [4,4,4,4],
+        nbPpnInconnus: [4,4,4,4],
+        nbPpnErreurs: [4,4,4,4],
+        nbPpnOk: [4,4,4,4],
       }
     ],
   },
   {
-    date: "2022/12/01 16h21",
+    date: "2022/12/01 15h32",
+    analyse: {
+      ppnValidsList: [],
+      ppnInvalidsList: [],
+      analyseSelected: [],
+      familleDocumentSet: [],
+      ruleSet: []
+    },
+    resultats : [
+      {
+        nbPpnTotal: [1],
+        nbPpnInconnus: [1],
+        nbPpnErreurs: [1],
+        nbPpnOk: [1],
+      },
+      {
+        nbPpnTotal: [2,2],
+        nbPpnInconnus: [2,2],
+        nbPpnErreurs: [2,2],
+        nbPpnOk: [2,2],
+      },
+      {
+        nbPpnTotal: [3,3,3],
+        nbPpnInconnus: [3,3,3],
+        nbPpnErreurs: [3,3,3],
+        nbPpnOk: [3,3,3],
+      }
+    ],
+  },
+  {
+    date: "2022/12/01 16h51",
     analyse: {
       ppnValidsList: [],
       ppnInvalidsList: [],
