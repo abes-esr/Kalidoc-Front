9615b8013e58aab03cb39deb8dba631bfcccb98a
pierre-maraval <pierre.maraval@gmail.com>
2022-12-15 09:47:58 +0100

    ODS-493 : Ajout appel axions récupération statuts Ajout bouton affichage statut dans header Ajout Overlay pour affichage des informations

--- src/views/01-header/Header.vue
+++ src/views/01-header/Header.vue
@@ -32,6 +32,29 @@
                </v-btn>
              </a>
            </v-col>
+           <v-col class="ml-0 mr-2 pr-2">
+              <v-hover v-slot="{ hover }" close-delay="300">
+                <div>
+                  <v-btn fab small depressed color="white">
+                    <v-icon>mdi-gauge</v-icon>
+                  </v-btn>
+                  <v-overlay
+                      :value="overlay"
+                      v-model="hover"
+                      class="d-flex flex-column"
+                  >
+                    <v-card width="400">
+                      <v-card-title class="justify-center" style="background-color: #252C61">STATUS</v-card-title>
+                      <v-card-text style="background-color: white;color: black">
+                        <v-row><v-col cols="12">Base Xml</v-col><v-col cols="6">{{applicationStatus.statutBaseXml}}</v-col></v-row>
+                        <v-row><v-col cols="12">Base Qualimarc</v-col><v-col cols="6">{{applicationStatus.statutBaseQualimarc}}</v-col></v-row>
+                        <v-row><v-col cols="12">Dernière synchro CBS<->Base XML</v-col><v-col cols="6">{{applicationStatus.dateDerniereSynchronisation}}</v-col></v-row>
+                      </v-card-text>
+                    </v-card>
+                  </v-overlay>
+                </div>
+              </v-hover>
+           </v-col>
          </v-row>
        </v-col>
       </v-row>
@@ -39,7 +62,27 @@
 </template>
 
 <script setup>
+import {ref} from "vue";
 import { useRoute } from "vue-router/composables"
+import StatutsService from "@/service/StatutsService";
 
 const route = useRoute();
+const service = StatutsService;
+const emit = defineEmits(['backendError']);
+const overlay = ref(false);
+
+let applicationStatus = ref({});
+
+function displayStatus() {
+  console.log("appel ws");
+  service.getStatusApplication().then((response) => {
+    applicationStatus.value = response.data;
+  }).catch((error) => {
+    emitOnError(error);
+  });
+}
+
+function emitOnError(error){
+  emit('backendError', error);
+}
 </script>
