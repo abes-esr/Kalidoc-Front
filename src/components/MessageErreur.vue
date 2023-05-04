936e47b5276e5f1501f7aca4f4f14bdc931ff84e
quetin <quetin@abes.fr>
2023-05-04 15:59:01 +0200

    fix string

--- src/components/MessageErreur.vue
+++ src/components/MessageErreur.vue
@@ -1,7 +1,7 @@
 <template>
   <v-alert v-if="backendErrorMessage !== null || messageError !== null" border="left" colored-border type="error" elevation="2">
       <div v-if="backendErrorMessage">
-        {{ (backendErrorMessage.response.data.message ? backendErrorMessage.response.data.message : '' ) +'(' + backendErrorMessage.response.statusText + backendErrorMessage.response.status + ')' }}
+        {{ (backendErrorMessage.response.data.message ? backendErrorMessage.response.data.message : '' ) +'(' + backendErrorMessage.response.statusText + ' : ' + backendErrorMessage.response.status + ')' }}
       </div>
       <div v-if="messageError">
         {{ messageError }}
